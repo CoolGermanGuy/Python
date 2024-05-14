4e1519ec16558417c333717615abc975871b2ff4
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2024-05-14 15:48:28 +0200

    Update sortWords.py

--- Excercises /sortWords.py
+++ Excercises /sortWords.py
@@ -1,5 +1,10 @@
+import re
 exampleArray = ["apple", "banana", "CoolGermanGuy", "I", "don't", "LIKE", "ThE", "gOvErNmEnT"]
 def sortWordsAlphabetically(array):
   # remove special characters
+  for i in array:
+    array[i] = re.sub(r'[^a-zA-Z0-9\s]', '', array[i])
   # make everything lowercase
+  for i in array:
+    array[i] = array[i].lower()
   # sort em
